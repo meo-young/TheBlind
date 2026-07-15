d79a20dd9b2b37a9b0432f69d0cb464905d82399
meo-young <meoyoung000@naver.com>
2026-07-15 22:59:27 +0900

    [Update] 초기 아트 컨셉 세팅

---
+++ Source/TheBlind/Camera/TBSpringArmComponent.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "TBSpringArmComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class THEBLIND_API UTBSpringArmComponent : public USpringArmComponent
+{
+	GENERATED_BODY()
+	
+};
