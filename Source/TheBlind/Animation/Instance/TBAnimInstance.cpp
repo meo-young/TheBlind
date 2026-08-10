ce46f3a7b53df76820d33f2e163eb5409135a0d2
meo-young <meoyoung000@naver.com>
2026-08-10 15:00:23 +0900

    [Update] Subsurface Light 구현

---
+++ Source/TheBlind/Animation/Instance/TBAnimInstance.cpp
@@ -0,0 +1,20 @@
+﻿#include "TBAnimInstance.h"
+#include "Character/TBCharacter.h"
+
+void UTBAnimInstance::OnInit_Implementation()
+{
+	Super::OnInit_Implementation();
+	
+	if (AActor* Owner = GetOwningActor())
+	{
+		if (ATBCharacter* IrisCharacter = Cast<ATBCharacter>(Owner))
+		{
+			InitializeWithAbilitySystem(IrisCharacter->GetAbilitySystemComponent());
+		}
+	}
+}
+
+void UTBAnimInstance::InitializeWithAbilitySystem(UAbilitySystemComponent* ASC)
+{
+	GameplayTagPropertyMap.Initialize(this, ASC);
+}
---
+++ Source/TheBlind/Animation/Instance/TBAnimInstance.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "PaperZDAnimInstance.h"
+#include "TBAnimInstance.generated.h"
+
+UCLASS()
+class THEBLIND_API UTBAnimInstance : public UPaperZDAnimInstance
+{
+	GENERATED_BODY()
+	
+// ─────────────────────────────────────────────────────────────
+// AnimInstance Interface
+// ─────────────────────────────────────────────────────────────
+public:
+	virtual void OnInit_Implementation() override;
+	
+	
+// ─────────────────────────────────────────────────────────────
+// Initializer
+// ─────────────────────────────────────────────────────────────
+public:
+	void InitializeWithAbilitySystem(UAbilitySystemComponent* ASC);
+
+	
+// ─────────────────────────────────────────────────────────────
+// Tag
+// ─────────────────────────────────────────────────────────────
+protected:
+	/** Tag ↔ 프로퍼티 바인딩 맵 — Tag 상태 변화가 BlueprintReadOnly 변수에 자동 반영됩니다. */
+	UPROPERTY(EditDefaultsOnly, Category = "변수|태그")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+	
+	
+};
