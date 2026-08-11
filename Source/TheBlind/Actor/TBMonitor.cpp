7f6174baa492e51bef8b364306cb7273c9813481
meo-young <meoyoung000@naver.com>
2026-08-11 17:51:33 +0900

    [Add] 모니터 전용 액터 구현 및 레벨 적용

---
+++ Source/TheBlind/Actor/TBMonitor.cpp
@@ -0,0 +1,10 @@
+﻿#include "TBMonitor.h"
+
+ATBMonitor::ATBMonitor()
+{
+	// 모니터 메시를 생성합니다.
+	{
+		Monitor = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Monitor"));
+		Monitor->SetupAttachment(Mesh);
+	}
+}
---
+++ Source/TheBlind/Actor/TBMonitor.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Interact/TBPossessableActor.h"
+#include "TBMonitor.generated.h"
+
+UCLASS()
+class THEBLIND_API ATBMonitor : public ATBPossessableActor
+{
+	GENERATED_BODY()
+	
+// ─────────────────────────────────────────────────────────────	
+// Actor Interface
+// ─────────────────────────────────────────────────────────────	
+public:
+	ATBMonitor();
+	
+	
+// ─────────────────────────────────────────────────────────────	
+// Components
+// ─────────────────────────────────────────────────────────────
+protected:
+	/** 집 안의 여러 장소를 렌더링하는 모니터 컴포넌트입니다. */
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> Monitor;
+	
+};
